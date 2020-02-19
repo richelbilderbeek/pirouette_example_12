f31b9b459e12f7460426330ba141edc1fceb8690
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-19 14:29:42 +0100

    Allow script to run on Peregrine

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -7,6 +7,15 @@
 #   ./scripts/rerun.sh
 #
 #
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=pirouette_example_12
+#SBATCH --output=pirouette_example_12.log
+module load R
+
 rm -rf example_12_314
-Rscript example_12.R
+time Rscript example_12.R
 
